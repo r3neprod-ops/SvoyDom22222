c357da66c0b5e109eae4c742daa81876bcd80a72
r3neprod-ops <r3neprod@gmail.com>
2026-02-21 16:58:24 +0300

    fix modal progress overflow + add down payment step

---
+++ src/data/brand.js
@@ -0,0 +1,8 @@
+export default {
+  brand: 'СвойДом Луганск',
+  phoneDisplay: '+7 959 026 0036',
+  phoneHref: '+79590260036',
+  telegramUrl: 'https://t.me/REPLACE_ME',
+  ipLabel: 'ИП: ШЕВЧЕНКО ВЛАДИСЛАВ АНДРЕЕВИЧ',
+  ipInn: 'ИНН: 345969504210',
+};
