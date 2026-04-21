8f337f6b33c46a11fb6e39d6b2463745728a6825
r3neprod-ops <r3neprod@gmail.com>
2026-04-21 21:36:54 +0300

    Skip to final quiz step for consultation option

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.builder.io',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
 export default nextConfig;
