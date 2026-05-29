1fbb56c160f1559763f0d592171e6c0717f8847d
r3neprod-ops <r3neprod@gmail.com>
2026-05-29 18:46:08 +0300

    Bump landing cache for quiz deploy

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'svoydom-v2';
+const CACHE_NAME = 'svoydom-v3';
 const PRECACHE_URLS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
