5e88fa5a95d13eead4d29172d0a38f8ef93fc502
r3neprod-ops <r3neprod@gmail.com>
2026-05-30 17:52:19 +0300

    Trigger Timeweb deploy

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'svoydom-v4';
+const CACHE_NAME = 'svoydom-v5';
 const PRECACHE_URLS = ['/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
